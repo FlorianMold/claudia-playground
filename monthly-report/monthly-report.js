1139fb19a428ff8c1a5247863695cebc9c868632
Florian Mold <florian.mold@agnet.at>
2022-03-13 19:26:21 +0100

    feat(monthly-report): create endpoint to read and write monthly reports

--- monthly-report/monthly-report.js
+++ monthly-report/monthly-report.js
@@ -1,3 +1,47 @@
-exports.handler = function (event, context) {
-    context.succeed('hello world from monthly-report.js');
-};
+const ApiBuilder = require('claudia-api-builder');
+const apigClientFactory = require('aws-api-gateway-client').default;
+
+// Get the urls from environment variables.
+const transactionInvokeUrl = () => process.env.TRANSACTION_URL || 'https://dkxj452gl7.execute-api.eu-central-1.amazonaws.com/production';
+const customerInvokeUrl = () => process.env.CUSTOMER_URL || 'https://3f9r4wfb01.execute-api.eu-central-1.amazonaws.com/production';
+
+const transactionConfig = {invokeUrl: transactionInvokeUrl()};
+const transactionClient = apigClientFactory.newClient(transactionConfig);
+
+const customerConfig = {invokeUrl: customerInvokeUrl()};
+const customerClient = apigClientFactory.newClient(customerConfig);
+
+const api = new ApiBuilder();
+
+api.get('/monthly-report/{iban}', (request) => {
+    const month = request.queryString.month || '';
+    const year = request.queryString.year || '';
+
+    const promise1 = transactionClient.invokeApi(
+        {iban: 1234},
+        `/transactions/{iban}?month=${month}&year=${year}`,
+        'GET'
+    )
+    const promise2 = customerClient.invokeApi(
+        {iban: 1234},
+        '/customers/{iban}',
+        'GET'
+    );
+    return Promise.all([promise1, promise2]).then(data => {
+        const dataFromTransactionPromise = data[0].data;
+        const dataFromCustomerPromise = data[1].data;
+
+        return {
+            report: {
+                customer: {
+                    ...dataFromCustomerPromise
+                },
+                transactions: [
+                    ...dataFromTransactionPromise
+                ]
+            }
+        }
+    })
+});
+
+module.exports = api;
