ea54ec6892aa29501b17af6abd146bc2f3cd4f10
Florian Mold <florian.mold@agnet.at>
2022-03-13 14:09:22 +0100

    test(customers): add scripts for testing the api

---
+++ customers/test-requests/get-customer.sh
@@ -0,0 +1,1 @@
+curl https://3f9r4wfb01.execute-api.eu-central-1.amazonaws.com/production/customers/1234
---
+++ customers/test-requests/save-customer.sh
@@ -0,0 +1,5 @@
+curl -H "Content-Type: application/json" -X POST \
+-d '{"name":"Florian", "address": "Vienna", "iban": "2345"}' \
+https://3f9r4wfb01.execute-api.eu-central-1.amazonaws.com/production/customers
+
+
